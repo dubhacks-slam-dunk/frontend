d301614c6a7bd7ab5f0457f9c6d4e1cb0eca94af
martincai8 <martincai46@gmail.com>
2023-10-14 16:57:16 -0700

    Implement auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../utils/firebase';
+import { login, logout } from '../utils/auth';
 
 export default function Home() {
-  useEffect(() => {
-    // addData();
-  }, []);
+  const [user, loading, error] = useAuthState(auth);
 
-  return <div> Hello world </div>;
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
+  return (
+    <>
+      <div>Hello world</div>
+      {user ? (
+        <>
+          <div>You're logged in as: {user.displayName}</div>
+          <button onClick={logout}>LOGOUT</button>
+        </>
+      ) : (
+        <>
+          <div>You're not logged in.</div>
+          <button onClick={login}>LOGIN</button>
+        </>
+      )}
+    </>
+  );
 }
