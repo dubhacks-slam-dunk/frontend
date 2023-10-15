b0e23124ee0606ab443d2050da4f165014742a81
martincai8 <martincai46@gmail.com>
2023-10-15 01:38:03 -0700

    Test white bar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta name="apple-mobile-web-app-title" content="Friendition"></meta>
-        {/* <meta name="apple-mobile-web-app-status-bar-style" content="hidden"></meta> */}
+        <meta name="apple-mobile-web-app-status-bar-style" content="white"></meta>
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
       </Head>
       <body>
