f37083119b09fbbb42bc3bc444ceb65ef57b31e4
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-10-14 17:03:19 -0700

    trying to get home page to show up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import { Theme, ThemePanel } from "@radix-ui/themes";
-import App from "./_app";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import App from './_app';
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head />
-			<body>
-				<Theme
-					accentColor="crimson"
-					grayColor="sand"
-					radius="large"
-					scaling="100%"
-					panelBackground="solid"
-				>
-					<Main />
-					<NextScript />
-					{/* <ThemePanel /> */}
-				</Theme>
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Theme
+          accentColor="crimson"
+          grayColor="sand"
+          radius="large"
+          scaling="100%"
+          panelBackground="solid"
+        >
+          <Main />
+          <NextScript />
+          {/* <ThemePanel /> */}
+        </Theme>
+      </body>
+    </Html>
+  );
 }
