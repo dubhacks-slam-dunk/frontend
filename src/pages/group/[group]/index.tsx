e6aeca9012b99db37f61018ccc5c354c975506c2
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-10-15 04:42:13 -0700

    sending group map to melvin but also still have to finish home component side scrolling

--- src/pages/group/[group]/index.tsx
+++ src/pages/group/[group]/index.tsx
@@ -59,24 +59,16 @@ export default function Group(props: any) {
                         // src={user.image ? user.image : "A"} // user's google profile photo !!!
                         fallback="A"
                       />
-                  ))}
-                  <Avatar
-                    radius="full"
-                    src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-                    fallback="A"
-                  />
-                  <Avatar
-                    radius="full"
-                    src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-                    fallback="A"
-                  />
-                  <Avatar
-                    radius="full"
-                    src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-                    fallback="A"
-                  />
+                    ))}
                 </Flex>
-                <Button size="2" color="iris" variant="soft" onClick={() => {router.push(`/invite/${group}`)}}>
+                <Button
+                  size="2"
+                  color="iris"
+                  variant="soft"
+                  onClick={() => {
+                    router.push(`/invite/${group}`);
+                  }}
+                >
                   invite
                 </Button>
               </Flex>
