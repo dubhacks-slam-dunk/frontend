8af1f47b915f8330da1de5b62706566fa99aced8
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-10-14 17:13:51 -0700

    my stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import GroupCard from '@/components/GroupCard';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+export default function App() {
+  return (
+    <>
+      <GroupCard></GroupCard>
+    </>
+  );
 }
