a2fbae4498cc3392ee6e169bbf5290bfb427e808
martincai8 <martincai46@gmail.com>
2023-10-15 06:16:29 -0700

    Please

--- src/components/CreateGroup.tsx
+++ src/components/CreateGroup.tsx
@@ -1,16 +1,52 @@
 import { Flex, Text, TextField, IconButton, Button, Grid } from '@radix-ui/themes';
 import Image from 'next/image';
 import { ChevronLeftIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
+import { uploadImage } from '@/utils/image';
+import { addGroup } from '@/utils/groups-helpers';
 
 export default function CreateGroup(props: any) {
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [imageURL, setImageURL] = useState<string | null>(null);
+  const [groupName, setGroupName] = useState<string | null>(null);
+  const [editor, setEditor] = useState<string | null>(null);
+
   const closeForm = () => {
     // Close the form and call the onClose function
     props.onCloseCreateGroupForm();
   };
+
+  const handleEditorClick = (selectedEditor: string) => {
+    setEditor(selectedEditor);
+  };
+
+  // Handling file change
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setImageFile(file);
+      const url = URL.createObjectURL(file);
+      setImageURL(url);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!imageFile || !groupName || !editor) {
+      return;
+    }
+    try {
+      const imageUrl = await uploadImage(imageFile);
+      const output = JSON.stringify({ image: imageUrl, name: groupName, editor });
+      addGroup(output, );
+    } catch (error) {
+      console.error('Form submission failed:', error);
+    }
+  };
+
   return (
     <div className="font-dm w-11/12 mx-auto">
-      <Flex direction="column" className="mx-auto">
-        <Flex direction="row" align="start" className="mt-12">
+      <Flex direction="column" className="mx-auto mt-12 mb-12">
+        <Flex direction="row" align="center">
           <IconButton onClick={closeForm} variant="ghost">
             <ChevronLeftIcon width="35" height="35" />
           </IconButton>
@@ -31,15 +67,28 @@ export default function CreateGroup(props: any) {
           }}
         >
           <label className="px-4 py-2 flex items-center justify-center">
-            Add a Photo
-            <input type="file" className="hidden" />
+            {imageURL ? (
+              <img
+                src={imageURL}
+                alt="Uploaded Preview"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            ) : (
+              'Add a Photo'
+            )}
+            <input type="file" className="hidden" onChange={handleFileChange} />
           </label>
+          <input type="file" className="hidden" onChange={handleFileChange} />
         </Flex>
 
         <Text>name this group</Text>
         <TextField.Root className="mt-2 mb-2">
           <TextField.Slot></TextField.Slot>
-          <TextField.Input className="m-1 mx-2" placeholder="group name" />
+          <TextField.Input
+            className="m-1 mx-2"
+            placeholder="group name"
+            onChange={e => setGroupName(e.target.value)}
+          />
         </TextField.Root>
         <Text>how often do you want updates?</Text>
         <Flex direction="row" justify="between">
@@ -50,7 +99,11 @@ export default function CreateGroup(props: any) {
 
         <Text>pick an editor</Text>
         <Grid columns="2" gap="3" width="auto" height="auto">
-          <Button variant="outline" style={{ width: '170px', height: '150px', borderRadius: 20 }}>
+          <Button
+            variant={editor === 'Columnist' ? 'soft' : 'outline'}
+            onClick={() => handleEditorClick('Columnist')}
+            style={{ width: '170px', height: '150px', borderRadius: 20 }}
+          >
             <Flex direction="column" align="center" justify="center">
               <div>
                 <Image
@@ -64,7 +117,11 @@ export default function CreateGroup(props: any) {
               columnist
             </Flex>
           </Button>
-          <Button variant="outline" style={{ width: '170px', height: '150px', borderRadius: 20 }}>
+          <Button
+            variant={editor === 'Girlboss' ? 'soft' : 'outline'}
+            onClick={() => handleEditorClick('Girlboss')}
+            style={{ width: '170px', height: '150px', borderRadius: 20 }}
+          >
             <Flex direction="column" align="center" justify="center">
               <div>
                 <Image
@@ -78,7 +135,11 @@ export default function CreateGroup(props: any) {
               girlboss
             </Flex>
           </Button>
-          <Button variant="outline" style={{ width: '170px', height: '150px', borderRadius: 20 }}>
+          <Button
+            variant={editor === 'Showman' ? 'soft' : 'outline'}
+            onClick={() => handleEditorClick('Showman')}
+            style={{ width: '170px', height: '150px', borderRadius: 20 }}
+          >
             <Flex direction="column" align="center" justify="center">
               <div>
                 <Image
@@ -92,7 +153,11 @@ export default function CreateGroup(props: any) {
               hypeman
             </Flex>
           </Button>
-          <Button variant="outline" style={{ width: '170px', height: '150px', borderRadius: 20 }}>
+          <Button
+            variant={editor === 'Storyteller' ? 'soft' : 'outline'}
+            onClick={() => handleEditorClick('Storyteller')}
+            style={{ width: '170px', height: '150px', borderRadius: 20 }}
+          >
             <Flex direction="column" align="center" justify="center">
               <div>
                 <Image
@@ -109,10 +174,12 @@ export default function CreateGroup(props: any) {
         </Grid>
 
         <Text>
-          the editor is your newsletter’s unique voice! they’ll give a summary of all the highlights
-          at the start of every issue.
+          the editor is your newsletter&apos;s unique voice! they&apos;ll give a summary of all the
+          highlights at the start of every issue.
         </Text>
-        <Button style={{ backgroundColor: '#5B5BD6' }}>Create group</Button>
+        <Button onClick={handleSubmit} style={{ backgroundColor: '#5B5BD6' }}>
+          Create group
+        </Button>
       </Flex>
     </div>
   );
