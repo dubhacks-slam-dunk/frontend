6ca6852bea4c739ef703cd66863c650dd109abc5
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2023-10-14 20:58:17 -0700

    Fixed router position in authenticated/index

--- src/pages/authenticated/index.tsx
+++ src/pages/authenticated/index.tsx
@@ -1,16 +1,18 @@
 import Navbar from '@/components/Navbar';
 import { auth } from '@/utils/firebase';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const HomePage: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   const router = useRouter();
 
-  if (!user) {
-    router.push('/');
-  }
+  useEffect(() => {
+    if (!user) {
+      router.push('/');
+    }
+  }, [router, user]);
 
   return (
     <div>
